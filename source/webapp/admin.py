d2cae95e117eb976d34fdf8138ca28c3a7d149e7
Valentin Kim <valentink@yandex.ru>
2023-02-19 15:16:13 +0600

    Модели добавлены в панель админа

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from webapp.models import Categories, Goods
+
 # Register your models here.
+
+class Categories_Admin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    list_filter = ('id', 'name', 'description')
+    search_fields = ('name', 'description')
+    fields = ('name', 'description')
+
+admin.site.register(Categories, Categories_Admin)
+class Goods_Admin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'category', 'price', 'image')
+    list_filter = ('id', 'name', 'description', 'category', 'price', 'image')
+    search_fields = ('name', 'description', 'category', 'price')
+    fields = ('name', 'description', 'category', 'price', 'image')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
+
+admin.site.register(Goods, Goods_Admin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('finished', 'Сделано')]
+
+class Categories(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=False, help_text='Наименование', verbose_name="Наименование")
+    description = models.CharField(max_length=200, null=True, blank=True, help_text='Наименование категории', verbose_name="Описание")
+
+
+    def __str__(self):
+        return f"{self.name} - {self.description}"
+
+
+class Goods(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, help_text='Наименование', verbose_name="Наименование")
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Описание")
+    category = models.ForeignKey(to='webapp.Categories', verbose_name='Товар', null=False, blank=False, related_name='goods', on_delete=models.RESTRICT)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата и время обновления")
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    image = models.TextField(max_length=500, null=True, blank=True, help_text='Ссылка на изображение', verbose_name="Ссылка на изображение")
+
+    def __str__(self):
+        return f"{self.name} - {self.description} - {self.category} - {self.price} - {self.image} "
+
