1342185b28222e2fa28eaf2fb7f956a7889b38bd
Valentin Kim <valentink@yandex.ru>
2023-02-21 11:24:24 +0600

    Реализован функциона удаления товара, доработаны стили страниц.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from webapp.views.category_add_view import category_add_view
 from webapp.views.products_view import products_view
-from webapp.views.product_view import product_view
+from webapp.views.product_view import product_view, delete_product
 
 from webapp.views.product_add_view import product_add_view
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path('products', products_view, name='products'),
     path('products/add', product_add_view, name='product_add'),
     path('categories/add', category_add_view, name='category_add'),
-    path('products/<int:pk>', product_view, name='product_view')
+    path('products/<int:pk>', product_view, name='product_view'),
+    path('product/delete/<int:pk>', delete_product, name='product_delete'),
 ]
--- source/webapp/views/product_view.py
+++ source/webapp/views/product_view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from webapp.models import Products
 
@@ -7,3 +7,8 @@ def product_view(request, pk):
     return render(request, 'product.html', context={
         'product': product
     })
+
+def delete_product(request, pk):
+    to_do = get_object_or_404(Products, pk=pk)
+    to_do.delete()
+    return redirect('products')
